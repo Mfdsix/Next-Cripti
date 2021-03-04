233e55a28dd4d7e8612d323c95dbd992d8666d27
Mahfudz Ainur Rif'an <Mfdsix.1nd0@gmail.com>
2021-03-04 15:03:26 +0700

    add IDR formatting

--- pages/index.js
+++ pages/index.js
@@ -1,101 +1,111 @@
   import {
-  Navbar, Nav, Container, Jumbotron, Button, Row, Col
+    Navbar, Nav, Container, Jumbotron, Button, Row, Col
   } from 'react-bootstrap'
   import 'bootstrap/dist/css/bootstrap.min.css'
   import axios from 'axios'
-
+  
   function Index({ cryptos }){
-  return(
-  <div>
-    <Navbar bg="dark" variant="dark">
+    return(
+      <div>
+      <Navbar bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand href="#home">Cripti</Navbar.Brand>
-        <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
-        </Nav>
+      <Navbar.Brand href="#home">Cripti</Navbar.Brand>
+      <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+      </Nav>
       </Container>
-    </Navbar>
-
-    <Jumbotron>
+      </Navbar>
+      
+      <Jumbotron>
       <Container>
-        <h1>Hello, devs!</h1>
-        <p>Let's see how much cripto prices with <b>cripti</b></p>
-        <p>
-          <Button variant="primary">Learn more</Button>
-        </p>
+      <h1>Hello, devs!</h1>
+      <p>Let's see how much cripto prices with <b>cripti</b></p>
+      <p>
+      <Button variant="primary">Learn more</Button>
+      </p>
       </Container>
-    </Jumbotron>
-
-    <Container className={ 'mb-4' }>
+      </Jumbotron>
+      
+      <Container className={ 'mb-4' }>
       <Row>
-        { cryptos.map((crypto) =>
+      { cryptos.map((crypto) =>
         <Col sm={3} className={ 'p-1' } key={ crypto.coinId } style={{ alignSelf: 'stretch' }}>
-        <div className={ 'bg-primary p-4 text-white text-center rounded mb-2' } style={{ height: '100%' }}>
-          <h4>{ crypto.coinName }</h4>
-          <p>{ crypto.kurs }</p>
+        <div className={ 'bg-primary p-4 text-white text-center rounded mb-2' } style={{ height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <div>
+        <h4>{ crypto.coinName }</h4>
+        <p>{ crypto.kurs }</p>
+        </div>
         </div>
         </Col>
         )}
-      </Row>
-    </Container>
-  </div>
-  )
-  }
-
-  export async function getStaticProps(context){
-
-  let coins = [
-  'bitcoin',
-  'ethereum',
-  'binancecoin',
-  'uniswap',
-  'havven',
-  ]
-  let destination = 'idr'
-
-  let cryptos = await loadPrices(coins, destination)
-  return{
-  props: {
-  cryptos: cryptos
-  },
-  revalidate: 1
-  }
-  }
-
-  async function loadPrices(coins, destination){
-  let cryptos = [];
-
-  let request = await
-  axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=${coins.join(",")}&vs_currencies=${destination}`)
-  let responses = request.data
-
-  for(let key of Object.keys(responses)){
-  cryptos.push({
-  coinId: key,
-  coinName: await loadCoin(key),
-  kurs: responses[key]['idr']
-  })
-  }
-
-  return cryptos
-  }
-
-  async function loadCoin(coindId){
-  try{
-  let request = await axios.get(`https://api.coingecko.com/api/v3/coins/${coindId}`)
-  let response = request.data
-
-  if(response){
-  return response.name
-  }else{
-  return coindId
-  }
-  }catch(e){
-  console.log(e)
-  return coindId
-  }
-  }
-
-  export default Index
+        </Row>
+        </Container>
+        </div>
+        )
+      }
+      
+      export async function getStaticProps(context) {
+        
+        let coins = [
+          'bitcoin',
+          'ethereum',
+          'binancecoin',
+          'uniswap',
+          'havven',
+        ]
+        let destination = 'idr'
+        
+        let cryptos = await loadPrices(coins, destination)
+        return {
+          props: {
+            cryptos: cryptos
+          },
+          revalidate: 1
+        }
+      }
+      
+      async function loadPrices(coins, destination) {
+        let cryptos = [];
+        
+        let request = await
+        axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=${coins.join(",")}&vs_currencies=${destination}`)
+        let responses = request.data
+        
+        for (let key of Object.keys(responses)) {
+          cryptos.push({
+            coinId: key,
+            coinName: await loadCoin(key),
+            kurs: convertToIDR(responses[key]['idr'])
+          })
+        }
+        
+        return cryptos
+      }
+      
+      async function loadCoin(coindId) {
+        try {
+          let request = await axios.get(`https://api.coingecko.com/api/v3/coins/${coindId}`)
+          let response = request.data
+          
+          if (response) {
+            return response.name
+          } else {
+            return coindId
+          }
+        } catch (e) {
+          console.log(e)
+          return coindId
+        }
+      }
+      
+      function convertToIDR(number)
+      {
+        var rupiah = '';		
+        var angkarev = number.toString().split('').reverse().join('');
+        for(var i = 0; i < angkarev.length; i++) if(i%3 == 0) rupiah += angkarev.substr(i,3)+'.';
+        return 'IDR '+rupiah.split('',rupiah.length-1).reverse().join('');
+      }
+      
+      export default Index
