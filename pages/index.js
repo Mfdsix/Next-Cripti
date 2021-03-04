9b6ea38c6bfeb7c9846ba78b1de3c424671c5428
Mahfudz Ainur Rif'an <Mfdsix.1nd0@gmail.com>
2021-03-04 20:50:10 +0700

    add trending coins section

--- pages/index.js
+++ pages/index.js
@@ -5,27 +5,27 @@
   import 'bootstrap/dist/css/bootstrap.min.css'
   import axios from 'axios'
   
-  function Index({ cryptos }){
+  function Index({ cryptos, trends }){
     return(
       <div>
-        <Head>
-          <title>Home</title>
-          <meta name="title" content="Cripti Home"/>
-          <meta name="description" content="Cripti is a website that show you exchange rate prices of many cryptocurrency coins"/>
-          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        </Head>
+      <Head>
+      <title>Home</title>
+      <meta name="title" content="Cripti Home"/>
+      <meta name="description" content="Cripti is a website that show you exchange rate prices of many cryptocurrency coins"/>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Navbar bg="dark" variant="dark">
       <Container>
       <Navbar.Brand href="#home">Cripti</Navbar.Brand>
       <Nav className="mr-auto">
       <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
+      <Nav.Link href="#prices">Prices</Nav.Link>
+      <Nav.Link href="#trending">Trending</Nav.Link>
       </Nav>
       </Container>
       </Navbar>
       
-      <Jumbotron>
+      <Jumbotron id={ 'home' }>
       <Container>
       <h1>Hello, devs!</h1>
       <p>Let's see how much cripto prices with <b>cripti</b></p>
@@ -35,7 +35,8 @@
       </Container>
       </Jumbotron>
       
-      <Container className={ 'mb-4' }>
+      <Container className={ 'mb-4' } id={ 'prices' }>
+      <h4>Exchange Rates</h4>
       <Row>
       { cryptos.map((crypto) =>
         <Col sm={3} className={ 'p-1' } key={ crypto.coinId } style={{ alignSelf: 'stretch' }}>
@@ -49,6 +50,30 @@
         )}
         </Row>
         </Container>
+
+        <Container className={ 'mb-4' } id={ 'trending' }>
+        <h4>Trending Coins</h4>
+      <Row>
+      { trends.map((coin) =>
+        <Col sm={3} className={ 'p-1' } key={ coin.id } style={{ alignSelf: 'stretch' }}>
+        <div className={ coin.color + ' p-4 text-white text-center rounded mb-2' } style={{ height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <div>
+        <img
+          src={ coin.large }
+          alt={ coin.name }
+          height={ 50 }
+          width={ 50 }/>
+          <hr/>
+        <h4>{ coin.name }</h4>
+        <p>{ coin.kurs }</p>
+        <p>Market Rank : { coin.market_cap_rank }</p>
+        </div>
+        </div>
+        </Col>
+        )}
+        </Row>
+        </Container>
+        
         </div>
         )
       }
@@ -73,11 +98,30 @@
         let destination = 'idr'
         
         let cryptos = await loadPrices(coins, destination)
+        let trends = await loadTrends()
+        
         return {
           props: {
-            cryptos: cryptos
+            cryptos: cryptos,
+            trends: trends
           },
           revalidate: 1
+        }
+      }
+      
+      async function loadTrends(){
+        let coins = [];
+        
+        try{
+          let request = await axios.get(`https://api.coingecko.com/api/v3/search/trending`)
+          let responses = request.data.coins
+          
+          coins = responses.map((coin) => coin = coin.item)
+          coins.map((coin) => coin.color = loadColor())
+          return coins
+        }catch(e){
+          console.log(e)
+          return coins
         }
       }
       
@@ -132,7 +176,7 @@
           return 'IDR '+ (rupiah.split('',rupiah.length-1).reverse().join(''));
         }
       }
-
+      
       function loadColor(){
         let colors = ['bg-primary', 'bg-success', 'bg-warning', 'bg-secondary'];
         return colors[Math.floor(Math.random() * colors.length)]
